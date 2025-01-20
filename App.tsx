c8cccaf3f617b4e9a709b9447e851c799d10f0fb
Bogdan <dvnkxxxx@gmail.com>
2025-01-20 21:35:03 +0200

    add: added routes enum

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from './src/screens/Home'; // Adjust as necessary
 
 import {enableScreens} from 'react-native-screens';
+import {Routes} from './src/utils/routes';
 enableScreens();
 
 const Stack = createStackNavigator();
@@ -12,8 +13,8 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator initialRouteName={Routes.HOME}>
+        <Stack.Screen name={Routes.HOME} component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
