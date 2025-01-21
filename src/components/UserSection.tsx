d0dfea1c39a98058bd0148c750b7070bf4ed61d0
Bogdan <dvnkxxxx@gmail.com>
2025-01-21 12:33:32 +0200

    add: userSection component

---
+++ src/components/UserSection.tsx
@@ -0,0 +1,60 @@
+import {TouchableOpacity, View, Image, Text, StyleSheet} from 'react-native';
+import {ASSETS} from '../utils/assets';
+import {AppColors} from '../utils/colors';
+import {FC} from 'react';
+
+type UserSectionProps = {
+  title: string;
+  subtitle: string;
+};
+
+const UserSection: FC<UserSectionProps> = ({title, subtitle}) => {
+  return (
+    <TouchableOpacity style={styles.user}>
+      <View>
+        <View style={styles.avatar}>
+          <Image style={styles.avatarImg} source={ASSETS.addAvatar} />
+        </View>
+      </View>
+      <View>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  user: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: AppColors.chinese_silver,
+  },
+  avatar: {
+    backgroundColor: 'rgba(136, 231, 136, 0.3)',
+    borderRadius: 50,
+    padding: 10,
+    marginRight: 10,
+    marginLeft: 16,
+  },
+  avatarImg: {
+    width: 20,
+    height: 20,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: AppColors.dark_charcoal,
+  },
+  subtitle: {
+    fontSize: 15,
+    color: AppColors.granite_gray,
+    marginTop: 4,
+  },
+});
+
+export default UserSection;
--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {AppColors} from '../../utils/colors';
 
-import {ASSETS} from '../../utils/assets';
 import SignUpForm from './components/SignUpForm';
-
-const UserSection = () => {
-  return (
-    <TouchableOpacity style={styles.user}>
-      <View>
-        <View style={styles.avatar}>
-          <Image style={styles.avatarEmoji} source={ASSETS.addAvatar} />
-        </View>
-      </View>
-      <View>
-        <Text style={styles.title}>Sign up</Text>
-        <Text style={styles.subtitle}>Personal Account</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
+import UserSection from '../../components/UserSection';
 
 const SignUp = () => {
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>
-        <UserSection />
+        <UserSection title="Sign Up" subtitle="Personal Account" />
         <SignUpForm />
       </View>
     </View>
@@ -47,36 +31,6 @@ const styles = StyleSheet.create({
     backgroundColor: AppColors.lotion,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
-  },
-  user: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'row',
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: AppColors.chinese_silver,
-  },
-  avatar: {
-    backgroundColor: 'rgba(136, 231, 136, 0.3)',
-    borderRadius: 50,
-    padding: 10,
-    marginRight: 10,
-    marginLeft: 16,
-  },
-  avatarEmoji: {
-    width: 20,
-    height: 20,
-  },
-  title: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    color: AppColors.dark_charcoal,
-  },
-  subtitle: {
-    fontSize: 15,
-    color: AppColors.granite_gray,
-    marginTop: 4,
   },
 });
 
